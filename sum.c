cb23f14a15eacec81ac2f54161ff6f083fd06f52
CarmenLeila <carmenmugisha18@gmail.com>
2023-12-13 07:14:02 -0800

    Some exercises on  super simple shell

--- sum.c
+++ sum.c
@@ -5,12 +5,15 @@
 int main(int ac, char **av)
 {
 	int  i = 0;
+	int sum = 0;
 
 	printf("argc or ac = %d\n", ac);
 	while (av[i] != NULL)
 	{
-		printf("Argument %d: %s\n", i, av[i]);
+		printf("Argument[%d] : %s\n", i, av[i]);
+		sum += atoi(av[i]);
 		i++;
 	}
-		return (0);
+	printf("sum is %d\n", sum);
+	return (0);
 }
